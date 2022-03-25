f0067d7525a0449a49f04f9b43d86845eed1c3a6
ac-krushna <krushna.bade@eukleiatech.com>
2022-03-25 12:16:56 +0530

    initial commit

---
+++ app/src/main/java/com/example/busmanagingmvvm/ui/auth/fragment/RegisterFragment.kt
@@ -0,0 +1,45 @@
+package com.example.busmanagingmvvm.ui.auth.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.busmanagingmvvm.R
+import com.example.busmanagingmvvm.databinding.FragmentRegisterBinding
+
+
+class RegisterFragment : Fragment() {
+    lateinit var binding:FragmentRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding= FragmentRegisterBinding.bind(inflater.inflate(R.layout.fragment_register, container, false))
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            RegisterFragment().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
